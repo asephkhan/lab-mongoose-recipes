bb8d06ef77d83886a5a981f63b8612dac0815c3f
Asif Ali <asephkhan@gmail.com>
2022-10-02 06:06:36 +0500

    lab completed

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    // insert many items in the database
+     Recipe.insertMany(data)
+      .then(recipe => console.log(`recipe is saved and its title is: `, recipe));
+    // find one item and update it
+    Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'},{ duration: 100 })
+      .then(recipe => console.log('recipe was found and duration has been updated', recipe))
+    // delete one item from the database
+    Recipe.deleteOne({title: 'Carrot Cake'})
+      .then(recipe => console.log('carrot has been deleted', recipe));
+   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
